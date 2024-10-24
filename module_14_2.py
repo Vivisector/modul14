e4902988cadf65df7ea796952df6735434010935
Dmitry <hulk295@yandex.ru>
2024-10-24 23:16:08 +0300

    ДЗ "Выбор элементов и функции в SQL запросах". Отработка функций SUM и fetchone()[0] - правка: удалил неиспольземую часть кода

--- module_14_2.py
+++ module_14_2.py
@@ -3,9 +3,6 @@ import sqlite3 as sq
 con = sq.connect('not_telegram.db')
 cur = con.cursor()
 
-# пришлось дропнуть для сброса primary-счетчика
-# cur.execute('DROP TABLE IF EXISTS Users;')
-
 cur.execute('''
     CREATE TABLE IF NOT EXISTS Users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,10 +13,6 @@ cur.execute('''
     )
 '''
             )
-
-users=cur.fetchall()
-for user in users:
-    print(f"Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
 
 cur.execute(('''
 DELETE FROM Users
@@ -35,7 +28,7 @@ print(f'Всего записей: {tot1}')
 cur.execute("SELECT SUM(balance) FROM Users")
 tot2 = cur.fetchone()[0]
 print(f'Сумма всех балансов: {tot2}')
-print(f'Средний баланс равен: {tot2/tot1}')
+print(f'Средний баланс: {tot2/tot1}')
 
 
 con.commit()
